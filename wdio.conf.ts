ba04c1954c056cd68c408b1f9e5d4347245fec2e
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2025-02-06 00:11:47 +0200

    Add initial Allure report configuration and data files

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -127,8 +127,12 @@ export const config: Options.Testrunner = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-
+    // reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
